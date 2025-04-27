5a6f9036ce7c49075fefc996a0529b538f4201ca
Valentin Charoux <valentin@charoux.com>
2025-04-27 06:42:20 +0200

    feat: Change hover color

--- src/theme.zig
+++ src/theme.zig
@@ -7,9 +7,9 @@ pub const background = struct {
 
 pub const entryItem = struct {
     pub const background = struct {
-        pub const selected: cl.Color = .{ 55, 161, 228, 255 };
         pub const default: cl.Color = .{ 19, 67, 98, 255 };
-        pub const hovered: cl.Color = .{ 55, 161, 228, 255 };
+        pub const selected: cl.Color = .{ 55, 161, 228, 255 };
+        pub const hovered: cl.Color = .{ 24, 95, 138, 255 };
     };
     pub const text = struct {
         pub const primary: cl.Color = .{ 250, 250, 255, 255 };
